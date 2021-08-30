4a68d7ea259072f21256583b28e2f4fc9a0d9a78
huaweiwei <harvey0379@163.com>
2021-08-30 18:45:37 +0800

    fix: json dir is not exist

--- generateJson.js
+++ generateJson.js
@@ -4,6 +4,11 @@ const path = require('path');
 module.exports = function (data, name = Date.now()) {
   content = JSON.stringify(data, null, '  ');
 
+  const jsonPath = path.join(__dirname, 'json');
+
+  if (!fs.existsSync(jsonPath)) {
+    fs.mkdirSync(jsonPath);
+  }
   const file = path.join(__dirname, `json/${name}.json`);
 
   fs.writeFile(file, content, function (err) {
