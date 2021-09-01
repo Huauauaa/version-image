9b2276910f60e3e3841c2d2e5638efd6ea82fa4a
huaweiwei <harvey0379@163.com>
2021-09-01 13:22:58 +0800

    Radial

--- main.js
+++ main.js
@@ -15,27 +15,30 @@ const graph = new G6.TreeGraph({
   container: 'container',
   width,
   height,
+  linkCenter: true,
   modes: {
-    default: ['collapse-expand', 'drag-canvas', 'zoom-canvas'],
+    default: [
+      {
+        type: 'collapse-expand',
+        onChange: function onChange(item, collapsed) {
+          const data = item.get('model');
+          data.collapsed = collapsed;
+          return true;
+        },
+      },
+      'drag-canvas',
+      'zoom-canvas',
+    ],
+  },
+  defaultNode: {
+    size: fontSize,
   },
   layout: {
-    type: 'compactBox',
+    type: 'dendrogram',
     direction: 'LR',
-    getId: function getId(d) {
-      return d.id;
-    },
-    getHeight: function getHeight() {
-      return 16;
-    },
-    getVGap: function getVGap() {
-      return 10;
-    },
-    getHGap: function getHGap() {
-      return 100;
-    },
-    getWidth: function getWidth(d) {
-      return G6.Util.getTextSize(d.id, fontSize)[0] + 20;
-    },
+    nodeSep: 20,
+    rankSep: 100,
+    radial: true,
   },
 });
 
@@ -43,23 +46,19 @@ graph.edge((edge) => {
   return {
     id: edge.id,
     type: 'cubic-horizontal',
-    style: {
-      stroke: lineColors[edge.target],
-    },
+    // style: {
+    //   stroke: lineColors[edge.target],
+    // },
   };
 });
 
-graph.node((node) => {
+graph.node(function (node) {
   return {
-    label: node.label,
-    labelCfg: {
-      offset: 10,
-      position: node.children && node.children.length > 0 ? 'left' : 'right',
-    },
-    style: {
-      fill: node.nodeColor,
-      stroke: node.nodeColor,
-    },
+    label: node.id,
+    // style: {
+    //   fill: node.nodeColor,
+    //   stroke: node.nodeColor,
+    // },
   };
 });
 
@@ -67,10 +66,9 @@ graph.data(data);
 graph.render();
 graph.fitView();
 
-if (typeof window !== 'undefined') {
+if (typeof window !== 'undefined')
   window.onresize = () => {
     if (!graph || graph.get('destroyed')) return;
     if (!container || !container.scrollWidth || !container.scrollHeight) return;
     graph.changeSize(container.scrollWidth, container.scrollHeight);
   };
-}
